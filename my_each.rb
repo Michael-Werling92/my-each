63e0ee9d8e8ee955032282e9ce2155f40d2602d9
Michael <mfw6mw@virginia.edu>
2021-02-28 22:29:32 -0500

    commit tree 92c535ce9e2eec8b12e614d2d685f789046ab47b

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,30 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
+  end
+  array
+end
+
+def my_collect(array)
+  i=0
+  array1=[]
+  while i < array.length
+    array1<<yield(array[i])
+    i += 1
+  end
+  array1
+end
+
+def my_select (collection)
+  i = 0
+  array2 = []
+  while i < collection.length
+    if yield (collection[i])== true
+      array2<<collection[i]
+    end
+    i += 1
+  end
+  array2
 end
